8781071b5dfbc4a6863e6a8333293c06461e74e2
librarian.co <126692501+libabiba@users.noreply.github.com>
2023-05-16 10:30:53 +0300

    Add files via upload

---
+++ bruh.pas
@@ -0,0 +1,25 @@
+﻿var agA, agB, agC:longint;
+var arN:array[1..3] of string;
+var arA:array[1..3] of integer;
+var maxn, i:longint;
+var nA, nB, nC:string;
+begin
+write('Введите имя и возраст: ');
+read(nA, agA, nB, agB, nC, agC);
+
+arN[1]:=nA;
+arN[2]:=nB;
+arN[3]:=nC;
+
+arA[1]:=agA;
+arA[2]:=agB;
+arA[3]:=agC;
+
+maxn:= max(arA[1],arA[2],arA[3]);
+for i:=1 to 3 do
+begin
+if arA[i] = 12 then
+write(arN[i], ' ', arA[i], ' ')
+end;
+
+end.
